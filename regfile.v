3206e69e26d25a0354830ab9916ab855d3b1ae22
greatwall1995 <greatwall1995@126.com>
2016-09-04 20:45:23 +0800

    不不不，这才是五级流水

--- regfile.v
+++ regfile.v
@@ -33,7 +33,7 @@ reg[`RegBus]  regs[0:`RegNum-1];
     always @ (posedge clk) begin  
       if (rst == `RstDisable) begin  
         if((we == `WriteEnable) && (waddr != `RegNumLog2'h0)) begin  
-        regs[waddr] <= wdata;  
+			regs[waddr] <= wdata;  
         end  
       end  
     end  
