f532ef77eda3461d41391df496d6f229f1c2e432
greatwall1995 <greatwall1995@126.com>
2016-09-08 21:31:33 +0800

    add test cases

--- testcase/arithmetic/arithmetic.s
+++ testcase/arithmetic/arithmetic.s
@@ -1,49 +1,22 @@
-.text 
-main:  
-
-   ori  $4,$0,0x8000      
-   sll  $4,$4,16          
-   ori  $4,$4,0x0010      
-  
-   ori  $2,$0,0x8000      
-   sll  $2,$2,16          
-   ori  $2,$2,0x0001      
-  
-   ori  $3,$0,0x0000      
-   addu $3,$2,$4          
-   ori  $3,$0,0x0000      
-   add  $3,$2,$4          
-                          
-  
-   sub   $3,$4,$3         
-   subu  $3,$3,$2         
-  
-   addi $3,$3,2           
-   ori  $3,$0,0x0000      
-   addiu $3,$3,0x8000
-   or   $4,$0,0xffff      
-   sll  $4,$4,16          
-   slt  $2,$4,$0          
-   sltu $2,$4,$0          
-   slti $2,$4,0x8000      
-   sltiu $2,$4,0x8000     
-   lui $4,0x0000          
-   #clo $2,$4              
-   #clz $2,$4       
-   lui $4,0xffff          
-   ori $4,$4,0xffff       
-   #clz $2,$4              
-   #clo $2,$4       
-   lui $4,0xa100          
-   #clz $2,$4              
-   #clo $2,$4      
-   lui $4,0x1100          
-   #clz $2,$4              
-   #clo $2,$4    
-   ori  $4,$0,0xffff      
-   sll  $4,$4,16  
-   ori  $4,$4,0xfffb      
-   ori  $2,$0,6           
-   mul  $3,$4,$2      
-   mult $4,$2      
-   multu $4,$2        
+.text
+main:
+	ori $8, $0, 2
+	ori $9, $0, 5
+	sub $8, $8, $9
+	add $10, $8, $9
+	addu $11, $8, $9
+	sub $12, $8, $9
+	subu $13, $8, $9
+	addi $14, $8, 4
+	mult $8, $9
+	mfhi $16
+	mflo $17
+	multu $8, $9
+	mfhi $18
+	mflo $19
+	div $8, $9
+	mfhi $20
+	mflo $21
+	divu $8, $9
+	mfhi $22
+	mflo $23
